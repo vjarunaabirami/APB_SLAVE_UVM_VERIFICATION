4799c34ee39294c638d528a6e0afbadd9b08007a
Aruna Abirami V J <arunaabirami@feserver.mirafra.com>
2025-12-07 16:20:23 +0530

    deleted all files

--- src/apb_slave_sequencer.sv
+++
@@ -1,9 +0,0 @@
-
-class apb_slave_sequencer extends uvm_sequencer #(apb_slave_sequence_item);
-  `uvm_component_utils(apb_slave_sequencer)
-
-  function new(string name = "apb_slave_sequencer", uvm_component parent);
-    super.new(name, parent);
-  endfunction
-endclass
-
--- src/defines.sv
+++
@@ -1,3 +0,0 @@
- `define ADDR_WIDTH 8
-`define DATA_WIDTH 256
-`define MEM_DEPTH  256
